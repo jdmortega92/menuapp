5536a3863281351ca51b70f1f02bd5d14069f101
jdmortega92 <jdmortega92@gmail.com>
2026-03-17 20:16:21 -0500

    primer commit - menuapp

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        MenuApp 🍽️
+      </h1>
+    </div>
+  )
+}
